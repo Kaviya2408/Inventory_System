260fe70c545b92b7ea70721a240d5e6fbaeed72c
Kaviya2408 <kit27.csbs29@gmail.com>
2026-06-22 13:44:20 +0530

    Update

--- backend/server.js
+++ backend/server.js
@@ -22,6 +22,7 @@ app.use(cors({
   origin: ["http://localhost:5173", "http://localhost:3000"],
   credentials: true
 }));
+
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use(morgan('dev'));
